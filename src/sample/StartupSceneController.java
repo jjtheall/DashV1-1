38b469883a1ff5b1736a6365c4f99bb0363e311d
jjtheall <jtheall@quinnipiac.edu>
2021-11-03 16:30:42 -0400

    finished StartupSceneController, added methods for button functionality

--- src/sample/StartupSceneController.java
+++ src/sample/StartupSceneController.java
@@ -5,6 +5,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import javafx.scene.Node;
+import javafx.fxml.FXMLLoader;
 
 public class StartupSceneController {
 
@@ -18,4 +22,33 @@ public class StartupSceneController {
     private TextField correctionFactorField;
     @FXML
     private TextField targetBGField;
+
+    @FXML
+    private void sendData(ActionEvent event) throws IOException{
+        int ICRatio = Integer.parseInt(ICRatioField.getText());
+        int correctionFactor = Integer.parseInt(correctionFactorField.getText());
+        int targetBG = Integer.parseInt(targetBGField.getText());
+
+        Data d = new Data(ICRatio, correctionFactor, targetBG);
+
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.close();
+
+        root = FXMLLoader.load(getClass().getResource("StartupScene.fxml"));
+
+        stage.setUserData(d);
+
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void switchToBolusCalc(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("BolusCalc.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
