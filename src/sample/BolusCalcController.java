572bd7c54596fecbc3641eabc49565ea3ca6c38c
jjtheall <jtheall@quinnipiac.edu>
2021-11-03 16:32:07 -0400

    created BolusCalcController class

---
+++ src/sample/BolusCalcController.java
@@ -0,0 +1,18 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import javafx.scene.Node;
+import javafx.fxml.FXMLLoader;
+
+public class BolusCalcController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+}
