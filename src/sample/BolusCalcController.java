0f77d6697ecec932d3db8005d9f4eb4f6285f75b
jjtheall <jtheall@quinnipiac.edu>
2021-11-03 17:19:39 -0400

    finished BolusCalcController class, added methods for receiving Data object, calculating total bolus on button click, and starting insulin delivery which simply displays on screen that insulin is being delivered for now

--- src/sample/BolusCalcController.java
+++ src/sample/BolusCalcController.java
@@ -9,10 +9,52 @@ import javafx.event.ActionEvent;
 import java.io.IOException;
 import javafx.scene.Node;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class BolusCalcController {
 
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    private Data d;
+
+    @FXML
+    private TextField curBGField;
+    @FXML
+    private TextField carbsField;
+    @FXML
+    private Label totalBolusLabel;
+    @FXML
+    private Label bolusDeliveryStartLabel;
+
+    @FXML
+    private void receiveData(MouseEvent event){
+        stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        d = (Data) stage.getUserData();
+    }
+
+    @FXML
+    private void calculateBolus(ActionEvent event){
+        int curBG = Integer.parseInt(curBGField.getText());
+        int carbs = Integer.parseInt(carbsField.getText());
+
+        double carbsInsulin = ((double)carbs/d.getICRatio());
+        int bgDiff = curBG - d.getTargetBG();
+        //may have to update the way this is handled if bgDiff is +/- to implement reverse correction
+        //for calculations
+        double bgInsulin = ((double)bgDiff)/d.getCorrectionFactor();
+        double totalInsulin = Math.round((carbsInsulin + bgInsulin)*100)/100.0;
+
+        totalBolusLabel.setText("Total Bolus: " + totalInsulin + "U");
+
+    }
+
+    @FXML
+    private void startBolusDelivery(ActionEvent event){
+        bolusDeliveryStartLabel.setVisible(true);
+    }
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("StartupScene.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Omnipod Dash");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
