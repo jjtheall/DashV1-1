b77b791e8a01d3a69da2073a90d81d89513723d3
jjtheall <jtheall@quinnipiac.edu>
2021-11-04 19:25:39 -0400

    created BasalPlan object to hold basal rates for a full 24 hr day

---
+++ src/sample/BasalPlan.java
@@ -0,0 +1,250 @@
+package sample;
+
+public class BasalPlan {
+
+    private double midnightToOne;
+    private double oneToTwo;
+    private double twoToThree;
+    private double threeToFour;
+    private double fourToFive;
+    private double fiveToSix;
+    private double sixToSeven;
+    private double sevenToEight;
+    private double eightToNine;
+    private double nineToTen;
+    private double tenToEleven;
+    private double elevenToTwelve;
+    private double twelveToThirteen;
+    private double thirteenToFourteen;
+    private double fourteenToFifteen;
+    private double fifteenToSixteen;
+    private double sixteenToSeventeen;
+    private double seventeenToEighteen;
+    private double eighteenToNineteen;
+    private double nineteenToTwenty;
+    private double twentyToTwentyOne;
+    private double twentyOneToTwentyTwo;
+    private double twentyTwoToTwentyThree;
+    private double twentyThreeToMidnight;
+
+    public BasalPlan(double a, double b, double c, double d, double e, double f, double g, double h, double i,
+                     double j, double k, double l, double m, double n, double o, double p, double q, double r,
+                     double s, double t, double u, double v, double w, double x){
+        midnightToOne = a;
+        oneToTwo = b;
+        twoToThree = c;
+        threeToFour = d;
+        fourToFive = e;
+        fiveToSix = f;
+        sixToSeven = g;
+        sevenToEight = h;
+        eightToNine = i;
+        nineToTen = j;
+        tenToEleven = k;
+        elevenToTwelve = l;
+        twelveToThirteen = m;
+        thirteenToFourteen = n;
+        fourteenToFifteen = o;
+        fifteenToSixteen = p;
+        sixteenToSeventeen = q;
+        seventeenToEighteen = r;
+        eighteenToNineteen = s;
+        nineteenToTwenty = t;
+        twentyToTwentyOne = u;
+        twentyOneToTwentyTwo = v;
+        twentyTwoToTwentyThree = w;
+        twentyThreeToMidnight = x;
+    }
+
+    public double getMidnightToOne() {
+        return midnightToOne;
+    }
+
+    public void setMidnightToOne(double midnightToOne) {
+        this.midnightToOne = midnightToOne;
+    }
+
+    public double getOneToTwo() {
+        return oneToTwo;
+    }
+
+    public void setOneToTwo(double oneToTwo) {
+        this.oneToTwo = oneToTwo;
+    }
+
+    public double getTwoToThree() {
+        return twoToThree;
+    }
+
+    public void setTwoToThree(double twoToThree) {
+        this.twoToThree = twoToThree;
+    }
+
+    public double getThreeToFour() {
+        return threeToFour;
+    }
+
+    public void setThreeToFour(double threeToFour) {
+        this.threeToFour = threeToFour;
+    }
+
+    public double getFourToFive() {
+        return fourToFive;
+    }
+
+    public void setFourToFive(double fourToFive) {
+        this.fourToFive = fourToFive;
+    }
+
+    public double getFiveToSix() {
+        return fiveToSix;
+    }
+
+    public void setFiveToSix(double fiveToSix) {
+        this.fiveToSix = fiveToSix;
+    }
+
+    public double getSixToSeven() {
+        return sixToSeven;
+    }
+
+    public void setSixToSeven(double sixToSeven) {
+        this.sixToSeven = sixToSeven;
+    }
+
+    public double getSevenToEight() {
+        return sevenToEight;
+    }
+
+    public void setSevenToEight(double sevenToEight) {
+        this.sevenToEight = sevenToEight;
+    }
+
+    public double getEightToNine() {
+        return eightToNine;
+    }
+
+    public void setEightToNine(double eightToNine) {
+        this.eightToNine = eightToNine;
+    }
+
+    public double getNineToTen() {
+        return nineToTen;
+    }
+
+    public void setNineToTen(double nineToTen) {
+        this.nineToTen = nineToTen;
+    }
+
+    public double getTenToEleven() {
+        return tenToEleven;
+    }
+
+    public void setTenToEleven(double tenToEleven) {
+        this.tenToEleven = tenToEleven;
+    }
+
+    public double getElevenToTwelve() {
+        return elevenToTwelve;
+    }
+
+    public void setElevenToTwelve(double elevenToTwelve) {
+        this.elevenToTwelve = elevenToTwelve;
+    }
+
+    public double getTwelveToThirteen() {
+        return twelveToThirteen;
+    }
+
+    public void setTwelveToThirteen(double twelveToThirteen) {
+        this.twelveToThirteen = twelveToThirteen;
+    }
+
+    public double getThirteenToFourteen() {
+        return thirteenToFourteen;
+    }
+
+    public void setThirteenToFourteen(double thirteenToFourteen) {
+        this.thirteenToFourteen = thirteenToFourteen;
+    }
+
+    public double getFourteenToFifteen() {
+        return fourteenToFifteen;
+    }
+
+    public void setFourteenToFifteen(double fourteenToFifteen) {
+        this.fourteenToFifteen = fourteenToFifteen;
+    }
+
+    public double getFifteenToSixteen() {
+        return fifteenToSixteen;
+    }
+
+    public void setFifteenToSixteen(double fifteenToSixteen) {
+        this.fifteenToSixteen = fifteenToSixteen;
+    }
+
+    public double getSixteenToSeventeen() {
+        return sixteenToSeventeen;
+    }
+
+    public void setSixteenToSeventeen(double sixteenToSeventeen) {
+        this.sixteenToSeventeen = sixteenToSeventeen;
+    }
+
+    public double getSeventeenToEighteen() {
+        return seventeenToEighteen;
+    }
+
+    public void setSeventeenToEighteen(double seventeenToEighteen) {
+        this.seventeenToEighteen = seventeenToEighteen;
+    }
+
+    public double getEighteenToNineteen() {
+        return eighteenToNineteen;
+    }
+
+    public void setEighteenToNineteen(double eighteenToNineteen) {
+        this.eighteenToNineteen = eighteenToNineteen;
+    }
+
+    public double getNineteenToTwenty() {
+        return nineteenToTwenty;
+    }
+
+    public void setNineteenToTwenty(double nineteenToTwenty) {
+        this.nineteenToTwenty = nineteenToTwenty;
+    }
+
+    public double getTwentyToTwentyOne() {
+        return twentyToTwentyOne;
+    }
+
+    public void setTwentyToTwentyOne(double twentyToTwentyOne) {
+        this.twentyToTwentyOne = twentyToTwentyOne;
+    }
+
+    public double getTwentyOneToTwentyTwo() {
+        return twentyOneToTwentyTwo;
+    }
+
+    public void setTwentyOneToTwentyTwo(double twentyOneToTwentyTwo) {
+        this.twentyOneToTwentyTwo = twentyOneToTwentyTwo;
+    }
+
+    public double getTwentyTwoToTwentyThree() {
+        return twentyTwoToTwentyThree;
+    }
+
+    public void setTwentyTwoToTwentyThree(double twentyTwoToTwentyThree) {
+        this.twentyTwoToTwentyThree = twentyTwoToTwentyThree;
+    }
+
+    public double getTwentyThreeToMidnight() {
+        return twentyThreeToMidnight;
+    }
+
+    public void setTwentyThreeToMidnight(double twentyThreeToMidnight) {
+        this.twentyThreeToMidnight = twentyThreeToMidnight;
+    }
+}
