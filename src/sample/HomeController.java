1fdc745b67ebfc8c8224fff8b5e92b2b6c306a61
jjtheall <jtheall@quinnipiac.edu>
2021-11-16 13:25:12 -0500

    created HomeController class, switched scene order so that after StartupScene app goes to Home, and added functionality for buttons on home screen to navigate app

--- src/sample/HomeController.java
+++ src/sample/HomeController.java
@@ -10,8 +10,9 @@ import javafx.event.ActionEvent;
 import java.io.IOException;
 import javafx.scene.Node;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 
-public class StartupSceneController {
+public class HomeController {
 
     private Stage stage;
     private Scene scene;
@@ -19,43 +20,40 @@ public class StartupSceneController {
     private Data d;
 
     @FXML
-    private TextField ICRatioField;
-    @FXML
-    private TextField correctionFactorField;
-    @FXML
-    private TextField targetBGField;
+    private Label BasalLabel;
 
     @FXML
     private void receiveData(MouseEvent event){
         stage = (Stage)((Node) event.getSource()).getScene().getWindow();
         d = (Data) stage.getUserData();
+
+        //set BasalLabel text to current hourly rate
+        String curTime = java.time.LocalTime.now().toString();
+        int hour = Integer.parseInt(curTime.substring(0,2));
+        BasalLabel.setText("Current Basal Rate: " + d.getBasalPlan().getRate(hour) + "U/hr");
     }
 
     @FXML
-    private void sendData(ActionEvent event) throws IOException{
-        int ICRatio = Integer.parseInt(ICRatioField.getText());
-        int correctionFactor = Integer.parseInt(correctionFactorField.getText());
-        int targetBG = Integer.parseInt(targetBGField.getText());
-
-        d.setICRatio(ICRatio);
-        d.setCorrectionFactor(correctionFactor);
-        d.setTargetBG(targetBG);
-
+    private void switchToBasalInput(ActionEvent event)throws IOException{
+        root = FXMLLoader.load(getClass().getResource("BasalInput.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.close();
-
-        root = FXMLLoader.load(getClass().getResource("StartupScene.fxml"));
-
-        stage.setUserData(d);
-
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
     @FXML
-    private void switchToHome(ActionEvent event) throws IOException{
-        root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+    private void switchToStartupScene(ActionEvent event)throws IOException{
+        root = FXMLLoader.load(getClass().getResource("StartupScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void switchToBolusCalc(ActionEvent event)throws IOException{
+        root = FXMLLoader.load(getClass().getResource("BolusCalc.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
