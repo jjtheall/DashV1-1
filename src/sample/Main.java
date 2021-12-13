29c14fedcd4ae6c0344b7f591bf50b99aaf8ecc2
jjtheall <jtheall@quinnipiac.edu>
2021-12-12 20:30:13 -0500

    updated readme

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,7 +18,6 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-
         JSONParser jsonParser = new JSONParser();
 
         try(FileReader reader = new FileReader("data.json")){
